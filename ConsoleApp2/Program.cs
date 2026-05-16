2de0853a8e98175acb6cf4304355b70c1f1dd164
Rashil Siyad <rashilsiyad916@gmail.com>
2026-05-16 15:47:47 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    //class Person
+    //{
+    //    public string Name;
+
+    //    static void Main(string[] args)
+    //    {
+    //        Person p1 = new Person();
+    //        p1.Name = "Rashil";
+
+    //        Person p2 = p1;
+
+    //        p2.Name = "Angular";
+
+    //        Console.WriteLine(p1.Name);
+    //    }
+    //}
+
+    class Person {
+
+        public string Name;
+
+
+        //static void Main(string[] args) {
+
+        //    //Person P1 = new Person();
+        //    //P1.Name = "rashil";
+
+        //    //Person P2 = P1;
+
+        //    //P2.Name = "fayis ";
+
+        //    //Console.WriteLine(P1.Name);
+
+        //    //int age = 20;
+
+        //    //String result = age >= 18 ? "eligible for vote" : "note eligible for vote ";
+
+        //    //Console.WriteLine(result);
+
+        //    long number = 5000;
+
+        //    int value = (int)number;
+
+        //    Console.WriteLine(value);
+
+        //    string age= "20";
+
+        //    int numm = Convert.ToInt32(age);
+
+        //    Console.WriteLine(numm);
+
+        //    //int key = 100;
+
+        //    //string text = Convert.ToString(key);
+
+        //    //Console.WriteLine(text);
+
+        //    int num = 100;
+
+        //    string text = Convert.ToString(num);
+
+        //    Console.WriteLine(text);
+
+        //    string Value = "99.5";
+        //    double Num = double.Parse(Value);
+
+        //    Console.WriteLine(Num);
+
+        //    //string man = "abc";
+
+        //    //int num1 = int.Parse(man);
+        //    //Console.WriteLine(num1);
+
+        //    string text1 = "123";
+
+        //    bool result1 = int.TryParse(text1 , out int number1);
+
+        //    Console.WriteLine(result1);
+        //    Console.WriteLine(number1);
+
+
+
+
+        //}
+    }
+}
+
+
+
+
+
+
---
+++ ConsoleApp2/task-day1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class task_day1
+    {
+       
+
+            static void Main(string[]args) {
+
+
+            int a = 10;
+            int b = 20;
+
+            int add = a + b;
+
+            Console.WriteLine(add);
+
+                
+                
+        }
+    }
+}
