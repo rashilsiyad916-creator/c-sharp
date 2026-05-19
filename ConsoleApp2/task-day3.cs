f90b1e3239a0b897009e67aec89b748176f930d1
Rashil Siyad <rashilsiyad916@gmail.com>
2026-05-19 15:35:04 +0530

    programing diamond pattern

--- ConsoleApp2/task-day3.cs
+++ ConsoleApp2/task-day3.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main()
     {
+
+        //task 1 day 2
+
         int number = 13;
         bool isPrime = true;
 
@@ -34,5 +37,44 @@ class Program
         {
             Console.WriteLine(number + " is Not a Prime Number");
         }
+
+
+        //task 2 day 2 
+
+
+        Console.Write("Enter size: ");
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        // Upper part
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
     }
-}
