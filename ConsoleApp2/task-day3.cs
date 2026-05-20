ad23f69498844e838699389e418ad6d3eb76c0ec
Rashil Siyad <rashilsiyad916@gmail.com>
2026-05-20 11:58:17 +0530

    task-day 4

--- ConsoleApp2/task-day3.cs
+++ ConsoleApp2/task-day3.cs
@@ -2,79 +2,80 @@
 
 class Program
 {
-    static void Main()
-    {
+    //static void Main()
+    //{
 
-        //task 1 day 2
+    //    //task 1 day 2
 
-        int number = 13;
-        bool isPrime = true;
+    //    int number = 13;
+    //    bool isPrime = true;
 
-        // 0 and 1 are not prime
-        if (number <= 1)
-        {
-            isPrime = false;
-        }
-        else
-        {
-            // Check divisibility
-            for (int i = 2; i < number; i++)
-            {
-                if (number % i == 0)
-                {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
+    //    // 0 and 1 are not prime
+    //    if (number <= 1)
+    //    {
+    //        isPrime = false;
+    //    }
+    //    else
+    //    {
+    //        // Check divisibility
+    //        for (int i = 2; i < number; i++)
+    //        {
+    //            if (number % i == 0)
+    //            {
+    //                isPrime = false;
+    //                break;
+    //            }
+    //        }
+    //    }
 
-        // Output
-        if (isPrime)
-        {
-            Console.WriteLine(number + " is a Prime Number");
-        }
-        else
-        {
-            Console.WriteLine(number + " is Not a Prime Number");
-        }
-
-
-        //task 2 day 2 
+    //    // Output
+    //    if (isPrime)
+    //    {
+    //        Console.WriteLine(number + " is a Prime Number");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine(number + " is Not a Prime Number");
+    //    }
 
 
-        Console.Write("Enter size: ");
+    //    //task 2 day 2 
 
-        int n = Convert.ToInt32(Console.ReadLine());
 
-        // Upper part
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= n - i; j++)
-            {
-                Console.Write(" ");
-            }
+    //    Console.Write("Enter size: ");
 
-            for (int k = 1; k <= 2 * i - 1; k++)
-            {
-                Console.Write("*");
-            }
+    //    int n = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine();
-        }
+    //    // Upper part
+    //    for (int i = 1; i <= n; i++)
+    //    {
+    //        for (int j = 1; j <= n - i; j++)
+    //        {
+    //            Console.Write(" ");
+    //        }
 
-        // Lower part
-        for (int i = n - 1; i >= 1; i--)
-        {
-            for (int j = 1; j <= n - i; j++)
-            {
-                Console.Write(" ");
-            }
+    //        for (int k = 1; k <= 2 * i - 1; k++)
+    //        {
+    //            Console.Write("*");
+    //        }
 
-            for (int k = 1; k <= 2 * i - 1; k++)
-            {
-                Console.Write("*");
-            }
+    //        Console.WriteLine();
+    //    }
 
-            Console.WriteLine();
-        }
-    }
+    //    // Lower part
+    //    for (int i = n - 1; i >= 1; i--)
+    //    {
+    //        for (int j = 1; j <= n - i; j++)
+    //        {
+    //            Console.Write(" ");
+    //        }
+
+    //        for (int k = 1; k <= 2 * i - 1; k++)
+    //        {
+    //            Console.Write("*");
+    //        }
+
+    //        Console.WriteLine();
+    //    }
+    //}
+}
---
+++ ConsoleApp2/task-day4.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+
+
+{
+    //parent class
+     class Animal 
+    {
+        public string name;
+        public int age;
+
+        public void Speak()
+        {
+
+            Console.WriteLine("Animal Name" + 1);
+            Console.WriteLine("Animal Age " + 1);
+        
+        }
+
+
+    }
+
+    //child  class dog
+
+    class Dog : Animal {
+
+        public string breed;
+    }
+
+    //child class - cat 
+    class Cat : Animal {
+
+        public void Meow() {
+
+            Console.WriteLine("Meow !");
+        }
+    
+    }
+
+    internal class Task_day4 {
+
+        static void Main(string[] args) {
+
+            //Dog object 
+
+            Dog dog = new Dog();
+
+            dog.name = "Buddy";
+            dog.age = 3;
+            dog.breed = "labrador";
+
+
+            dog.Speak();
+
+            Console.WriteLine("Breed :" + dog.breed);
+            Console.WriteLine();
+
+
+            //Cat object 
+
+            Cat cat = new Cat();
+
+            cat.name = "kitty";
+            cat.age = 2;
+
+
+            cat.Speak();
+            cat.Meow();
+        
+        }
+    
+    }
+}
